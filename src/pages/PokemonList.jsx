8a8e312d819ea1520b4ba6d94178ed22077c0bb7
sehoon0529 <kim44415953@gmail.com>
2025-10-16 16:32:26 +0900

    🚀포켓몬 도감 완성🚀

--- src/pages/PokemonList.jsx
+++ src/pages/PokemonList.jsx
@@ -17,10 +17,15 @@ export default function PokemonList() {
                     const species = await getPokemonSpecies(detail.id);
                     const koreanName = species.names.find((n) => n.language.name === "ko")?.name;
 
+                    //console.log(detail.types)
+                    //console.log(detail.types.map((t) => t.type.name))
+
                     return {
                         id: detail.id,
                         name: koreanName || detail.name,
                         image: detail.sprites.other["official-artwork"].front_default,
+                        // types 배열에서 타입 이름만 추출하여 새로운 배열을 만들고 반환
+                        types: detail.types.map((t) => t.type.name),
                     }
                 })
             )
